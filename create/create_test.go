b91061e35c25501a7406ebe76e20f766cd76577a
Viktor Benei <viktor.benei@gmail.com>
2017-05-07 19:19:21 +0200

    test fix for generateIDFromString

--- create/create_test.go
+++ create/create_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func Test_generateIDFromString(t *testing.T) {
-	require.Equal(t, "a-simple-test", generateIDFromString("a-simple-test"))
-	require.Equal(t, "a-simple-test", generateIDFromString("a_simple_test"))
-	require.Equal(t, "a-simple-test", generateIDFromString("A simple Test"))
-	require.Equal(t, "a-simple-test", generateIDFromString("A simple.Test"))
-	require.Equal(t, "a-simple-test", generateIDFromString("A simple.Test."))
-	require.Equal(t, "a-simple-test", generateIDFromString("--A simple.Test.    --"))
-	require.Equal(t, "a-simple-test", generateIDFromString("    --A simple.Test.    --      "))
-	require.Equal(t, "a--simple-test", generateIDFromString("A  simple Test"))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("a-simple-test"))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("a_simple_test"))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("A simple Test"))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("A simple.Test"))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("A simple.Test."))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("--A simple.Test.    --"))
+	require.Equal(t, "bitrise-step-a-simple-test", generateIDFromString("    --A simple.Test.    --      "))
+	require.Equal(t, "bitrise-step-a--simple-test", generateIDFromString("A  simple Test"))
 }
