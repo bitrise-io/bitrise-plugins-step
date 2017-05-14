2f4d0c72b99804e91634612946e7d2974dc09e4e
Viktor Benei <viktor.benei@gmail.com>
2017-05-14 17:16:20 +0200

    Website and other URL questions (#17)
    * WIP: website & source question
    
    * Asking for website, code and support URLs, and including those in the generated step.yml
    
    Also add the git clone url as a remote after git init
    
    * refactoring the create code's flow
    
    added GitHub specific "quick detection/gen" (only have to provide the username)
    
    go toolkit can now use the website/source URL and auto detect the package name from that, unless there's an error in the detection (in which case it falls back to manual)
    
    * godep update
    
    * minor revision for isGithub=true note texts
    
    * include the SourceCodeURL in the bitrise.yml as `BITRISE_STEP_GIT_CLONE_URL`

--- create/create.go
+++ create/create.go
@@ -15,6 +15,7 @@ import (
 	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/bitrise-io/go-utils/templateutil"
 	"github.com/bitrise-io/goinp/goinp"
+	"github.com/bitrise-tools/gows/goutil"
 	"github.com/pkg/errors"
 )
 
@@ -38,6 +39,10 @@ type InventoryModel struct {
 	Description    string
 	PrimaryTypeTag string
 	//
+	WebsiteURL    string
+	SourceCodeURL string
+	SupportURL    string
+	//
 	ToolkitType        string
 	GoToolkitInventory GoToolkitInventoryModel
 	//
@@ -46,87 +51,158 @@ type InventoryModel struct {
 
 // Step ...
 func Step() error {
-	defaultAuthor := readAuthorFromGitConfig()
-	author, err := goinp.AskForStringWithDefault(colorstring.Green("Who are you / who's the author?"), defaultAuthor)
-	if err != nil {
-		return errors.Wrap(err, "Failed to determine author")
-	}
-
-	title, err := goinp.AskForString(colorstring.Green("What's the title / name of the Step?"))
-	if err != nil {
-		return errors.Wrap(err, "Failed to determine title")
-	}
-
-	id := generateIDFromString(title)
-	printInfoLine("Generated Step ID (from provided Title):", id)
-
-	summary, err := goinp.AskForString(colorstring.Green("Please provide a summary"))
-	if err != nil {
-		return errors.Wrap(err, "Failed to determine summary")
-	}
-	description, err := goinp.AskForString(colorstring.Green("Please provide a description"))
-	if err != nil {
-		return errors.Wrap(err, "Failed to determine description")
-	}
-
-	// available primary categories / type_tags:
-	// https://github.com/bitrise-io/bitrise/blob/master/_docs/step-development-guideline.md#step-grouping-convention
-	fmt.Println()
-	primaryTypeTag, err := goinp.SelectFromStrings(colorstring.Green("What's the primary category of this Step?"), []string{
-		"access-control", "artifact-info",
-		"installer", "deploy",
-		"utility", "dependency", "code-sign",
-		"build", "test", "notification",
-	})
-	if err != nil {
-		return errors.Wrap(err, "Failed to determine primary category")
-	}
-
-	fmt.Println()
-	fmt.Println("Toolkit: the entry/base language of the Step.")
-	fmt.Println("Our recommendation is to use Bash for very simple Steps")
-	fmt.Println(" and for more complex ones use another language, one which we have toolkit support for.")
-	fmt.Println("If you're just getting started with Step development our suggestion is to select Bash,")
-	fmt.Println(" as that's the easiest option. It's possible to convert the step later, if needed.")
-	fmt.Println("Note: Of course even if you select e.g. Bash as the entry language, you can run other scripts from there,")
-	fmt.Println(" so it's possible to write the majority of the step's code in e.g. Ruby,")
-	fmt.Println(" and have an entry Bash script which does nothing else except running the Ruby script.")
-	toolkitType, err := goinp.SelectFromStrings(colorstring.Green("Which toolkit (language) would you like to use?"), []string{
-		toolkitTypeBash, toolkitTypeGo,
-	})
-	if err != nil {
-		return errors.Wrap(err, "Failed to determine the toolkit")
-	}
-
-	goInv := GoToolkitInventoryModel{}
-	if toolkitType == toolkitTypeGo {
-		fmt.Println()
-		fmt.Println("Go programs require a Go package ID, in order to work well with the standard Go tools.")
-		fmt.Println("The package ID looks like this usually: SOURCE-CODE-HOSTING-SERVICE/user/package-name")
-		fmt.Println(" Example: github.com/bitrise-io/bitrise")
-		fmt.Println("If you (plan to) use GitHub for hosting this step's source code,")
-		fmt.Println("the suggested package name for this step is:",
-			colorstring.Yellow("github.com/YOUR-GITHUB-USERNAME/"+stepDirAndRepoNameFromID(id)))
-		goPkgID, err := goinp.AskForString(colorstring.Green("What should be the Go package ID?"))
-		if err != nil {
-			return errors.Wrap(err, "Failed to determine the package ID")
-		}
-		goInv.PackageID = goPkgID
-	}
-
-	return createStep(InventoryModel{
-		Author:         author,
-		Title:          title,
-		ID:             id,
-		Summary:        summary,
-		Description:    description,
-		PrimaryTypeTag: primaryTypeTag,
+	inventoryForCreateStep := InventoryModel{
+		Author:         "",
+		Title:          "",
+		ID:             "",
+		Summary:        "",
+		Description:    "",
+		PrimaryTypeTag: "",
 		//
-		ToolkitType:        toolkitType,
-		GoToolkitInventory: goInv,
+		WebsiteURL:    "",
+		SourceCodeURL: "",
+		SupportURL:    "",
+		//
+		ToolkitType: toolkitTypeBash,
+		GoToolkitInventory: GoToolkitInventoryModel{
+			PackageID: "",
+		},
 		//
 		Year: time.Now().Year(),
-	})
+	}
+
+	{
+		defaultAuthor := readAuthorFromGitConfig()
+		author, err := goinp.AskForStringWithDefault(colorstring.Green("Who are you / who's the author?"), defaultAuthor)
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine author")
+		}
+		inventoryForCreateStep.Author = author
+	}
+
+	{
+		title, err := goinp.AskForString(colorstring.Green("What's the title / name of the Step?"))
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine title")
+		}
+		inventoryForCreateStep.Title = title
+	}
+
+	{
+		id := generateIDFromString(inventoryForCreateStep.Title)
+		printInfoLine("Generated Step ID (from provided Title):", id)
+		inventoryForCreateStep.ID = id
+	}
+
+	{
+		summary, err := goinp.AskForString(colorstring.Green("Please provide a summary"))
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine summary")
+		}
+		inventoryForCreateStep.Summary = summary
+	}
+	{
+		description, err := goinp.AskForString(colorstring.Green("Please provide a description"))
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine description")
+		}
+		inventoryForCreateStep.Description = description
+	}
+
+	{
+		// available primary categories / type_tags:
+		// https://github.com/bitrise-io/bitrise/blob/master/_docs/step-development-guideline.md#step-grouping-convention
+		fmt.Println()
+		primaryTypeTag, err := goinp.SelectFromStrings(colorstring.Green("What's the primary category of this Step?"), []string{
+			"access-control", "artifact-info",
+			"installer", "deploy",
+			"utility", "dependency", "code-sign",
+			"build", "test", "notification",
+		})
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine primary category")
+		}
+		inventoryForCreateStep.PrimaryTypeTag = primaryTypeTag
+	}
+
+	{
+		fmt.Println()
+		fmt.Println("Toolkit: the entry/base language of the Step.")
+		fmt.Println("Our recommendation is to use Bash for very simple Steps")
+		fmt.Println(" and for more complex ones use another language, one which we have toolkit support for.")
+		fmt.Println("If you're just getting started with Step development our suggestion is to select Bash,")
+		fmt.Println(" as that's the easiest option. It's possible to convert the step later, if needed.")
+		fmt.Println("Note: Of course even if you select e.g. Bash as the entry language, you can run other scripts from there,")
+		fmt.Println(" so it's possible to write the majority of the step's code in e.g. Ruby,")
+		fmt.Println(" and have an entry Bash script which does nothing else except running the Ruby script.")
+		toolkitType, err := goinp.SelectFromStrings(colorstring.Green("Which toolkit (language) would you like to use?"), []string{
+			toolkitTypeBash, toolkitTypeGo,
+		})
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine the toolkit")
+		}
+		inventoryForCreateStep.ToolkitType = toolkitType
+	}
+
+	{
+		fmt.Println()
+		fmt.Println("Website & source code URL:")
+		isGitHub, err := goinp.AskForBoolWithDefault(colorstring.Green("Will you host the source code on GitHub?"), true)
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine whether source will be hosted on GitHub")
+		}
+		websiteURL := ""
+		if isGitHub {
+			ghUsername, err := goinp.AskForString(colorstring.Green("What's your GitHub username (user/org where you'll register the step's repository)?"))
+			if err != nil {
+				return errors.Wrap(err, "Failed to determine GitHub username")
+			}
+			websiteURL = fmt.Sprintf("https://github.com/%s/%s", ghUsername, stepDirAndRepoNameFromID(inventoryForCreateStep.ID))
+			fmt.Println("We'll use", colorstring.Yellow(websiteURL), "as the website/repo URL for this step.")
+			fmt.Println("Please when you create the repository on GitHub for the step")
+			fmt.Println(" create it under the user/org:", colorstring.Yellow(ghUsername))
+			fmt.Println(" and the name of the repository should be:", colorstring.Yellow(stepDirAndRepoNameFromID(inventoryForCreateStep.ID)))
+		} else {
+			fmt.Println("To use your step quickly in your bitrise configs, and in case you'll want to share it with others,")
+			fmt.Println(" you'll have to make the source code available on a git hosting service.")
+			fmt.Println("Please create a repository on your favorite source code hosting service,")
+			fmt.Println(" with the repository name:", colorstring.Yellow(stepDirAndRepoNameFromID(inventoryForCreateStep.ID)))
+			fmt.Println("Once created, please copy paste the repo's HTTPS URL.")
+			fmt.Println("If you create it on GitHub the HTTPS URL should look like this:")
+			fmt.Println(" " + colorstring.Yellow("https://github.com/YOUR-GITHUB-USERNAME/"+stepDirAndRepoNameFromID(inventoryForCreateStep.ID)))
+			websiteURL, err = goinp.AskForString(colorstring.Green("What's the step's repo (website) URL?"))
+			if err != nil {
+				return errors.Wrap(err, "Failed to determine the package ID")
+			}
+		}
+
+		inventoryForCreateStep.WebsiteURL = websiteURL
+		inventoryForCreateStep.SourceCodeURL = websiteURL
+		inventoryForCreateStep.SupportURL = websiteURL
+	}
+
+	if inventoryForCreateStep.ToolkitType == toolkitTypeGo {
+		if goPkgID, err := goutil.ParsePackageNameFromURL(inventoryForCreateStep.SourceCodeURL); err != nil {
+			fmt.Println()
+			fmt.Println(" [!] Failed to parse Go package ID from URL, error:", err)
+			fmt.Println()
+			fmt.Println("Go programs require a Go package ID, in order to work well with the standard Go tools.")
+			fmt.Println("The package ID looks like this usually: SOURCE-CODE-HOSTING-SERVICE/user/package-name")
+			fmt.Println(" Example: github.com/bitrise-io/bitrise")
+			fmt.Println("If you (plan to) use GitHub for hosting this step's source code,")
+			fmt.Println("the suggested package name for this step is:",
+				colorstring.Yellow("github.com/YOUR-GITHUB-USERNAME/"+stepDirAndRepoNameFromID(inventoryForCreateStep.ID)))
+			userInputGoPkgID, err := goinp.AskForString(colorstring.Green("What should be the Go package ID?"))
+			if err != nil {
+				return errors.Wrap(err, "Failed to determine the package ID")
+			}
+			inventoryForCreateStep.GoToolkitInventory.PackageID = userInputGoPkgID
+		} else {
+			inventoryForCreateStep.GoToolkitInventory.PackageID = goPkgID
+		}
+	}
+
+	return createStep(inventoryForCreateStep)
 }
 
 func readAuthorFromGitConfig() string {
@@ -256,7 +332,7 @@ func createStep(inventory InventoryModel) error {
 
 	fmt.Println()
 	fmt.Println(colorstring.Yellow("Initializing git repository in step directory ..."))
-	if err := initGitRepoAtPath(stepDirAbsPth); err != nil {
+	if err := initGitRepoAtPath(stepDirAbsPth, inventory.SourceCodeURL); err != nil {
 		return errors.Wrap(err, "Failed to initialize git repository in step directory")
 	}
 
@@ -271,11 +347,23 @@ func createStep(inventory InventoryModel) error {
 	return nil
 }
 
-func initGitRepoAtPath(dirPth string) error {
-	cmdLog, err := command.New("git", "init").SetDir(dirPth).RunAndReturnTrimmedCombinedOutput()
-	if err != nil {
-		return errors.Wrapf(err, "Failed to git init in directory (%s). Output: %s", dirPth, cmdLog)
+func initGitRepoAtPath(dirPth string, remoteURL string) error {
+	{
+		cmdGitInit := command.New("git", "init")
+		fmt.Println(" $", cmdGitInit.PrintableCommandArgs())
+		if cmdLog, err := cmdGitInit.SetDir(dirPth).RunAndReturnTrimmedCombinedOutput(); err != nil {
+			return errors.Wrapf(err, "Failed to 'git init' in directory (%s). Output: %s", dirPth, cmdLog)
+		}
 	}
+
+	{
+		cmdGitRemoteAdd := command.New("git", "remote", "add", "origin", remoteURL)
+		fmt.Println(" $", cmdGitRemoteAdd.PrintableCommandArgs())
+		if cmdLog, err := cmdGitRemoteAdd.SetDir(dirPth).RunAndReturnTrimmedCombinedOutput(); err != nil {
+			return errors.Wrapf(err, "Failed to 'git remote add origin %s'. Output: %s", remoteURL, cmdLog)
+		}
+	}
+
 	return nil
 }
 
