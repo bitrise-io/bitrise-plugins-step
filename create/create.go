f9d2f4eefe4352be9d8f95dc1623fc341d133a48
Viktor Benei <viktor.benei@gmail.com>
2017-05-14 11:37:23 +0200

    Go toolkit in "create" implemented
    Prints info about the toolkits, asks for which toolkit to use.
    
    If Go toolkit selected it also asks for the package ID/name.
    
    If Go toolkit : the step is generated into GOPATH, instead of just a relative dir.
    
    Evaluate minimal revision, to make it possible to unit test it + a step.yml template unit test added.
    
    generateIDFromString (which generates a step id) now keeps numbers instead of replacing them with `-`.

--- create/create.go
+++ create/create.go
@@ -23,8 +23,14 @@ const (
 	toolkitTypeGo   = "go"
 )
 
-// Inventory ...
-type Inventory struct {
+// GoToolkitInventoryModel ...
+type GoToolkitInventoryModel struct {
+	// PackageID: e.g.: github.com/bitrise-io/bitrise
+	PackageID string
+}
+
+// InventoryModel ...
+type InventoryModel struct {
 	Author         string
 	Title          string
 	ID             string
@@ -32,7 +38,8 @@ type Inventory struct {
 	Description    string
 	PrimaryTypeTag string
 	//
-	ToolkitType string
+	ToolkitType        string
+	GoToolkitInventory GoToolkitInventoryModel
 	//
 	Year int
 }
@@ -62,9 +69,9 @@ func Step() error {
 		return errors.Wrap(err, "Failed to determine description")
 	}
 
-	fmt.Println()
 	// available primary categories / type_tags:
 	// https://github.com/bitrise-io/bitrise/blob/master/_docs/step-development-guideline.md#step-grouping-convention
+	fmt.Println()
 	primaryTypeTag, err := goinp.SelectFromStrings(colorstring.Green("What's the primary category of this Step?"), []string{
 		"access-control", "artifact-info",
 		"installer", "deploy",
@@ -75,7 +82,39 @@ func Step() error {
 		return errors.Wrap(err, "Failed to determine primary category")
 	}
 
-	return createStep(Inventory{
+	fmt.Println()
+	fmt.Println("Toolkit: the entry/base language of the Step.")
+	fmt.Println("Our recommendation is to use Bash for very simple Steps")
+	fmt.Println(" and for more complex ones use another language, one which we have toolkit support for.")
+	fmt.Println("If you're just getting started with Step development our suggestion is to select Bash,")
+	fmt.Println(" as that's the easiest option. It's possible to convert the step later, if needed.")
+	fmt.Println("Note: Of course even if you select e.g. Bash as the entry language, you can run other scripts from there,")
+	fmt.Println(" so it's possible to write the majority of the step's code in e.g. Ruby,")
+	fmt.Println(" and have an entry Bash script which does nothing else except running the Ruby script.")
+	toolkitType, err := goinp.SelectFromStrings(colorstring.Green("Which toolkit (language) would you like to use?"), []string{
+		toolkitTypeBash, toolkitTypeGo,
+	})
+	if err != nil {
+		return errors.Wrap(err, "Failed to determine the toolkit")
+	}
+
+	goInv := GoToolkitInventoryModel{}
+	if toolkitType == toolkitTypeGo {
+		fmt.Println()
+		fmt.Println("Go programs require a Go package ID, in order to work well with the standard Go tools.")
+		fmt.Println("The package ID looks like this usually: SOURCE-CODE-HOSTING-SERVICE/user/package-name")
+		fmt.Println(" Example: github.com/bitrise-io/bitrise")
+		fmt.Println("If you (plan to) use GitHub for hosting this step's source code,")
+		fmt.Println("the suggested package name for this step is:",
+			colorstring.Yellow("github.com/YOUR-GITHUB-USERNAME/"+stepDirAndRepoNameFromID(id)))
+		goPkgID, err := goinp.AskForString(colorstring.Green("What should be the Go package ID?"))
+		if err != nil {
+			return errors.Wrap(err, "Failed to determine the package ID")
+		}
+		goInv.PackageID = goPkgID
+	}
+
+	return createStep(InventoryModel{
 		Author:         author,
 		Title:          title,
 		ID:             id,
@@ -83,7 +122,8 @@ func Step() error {
 		Description:    description,
 		PrimaryTypeTag: primaryTypeTag,
 		//
-		ToolkitType: toolkitTypeBash,
+		ToolkitType:        toolkitType,
+		GoToolkitInventory: goInv,
 		//
 		Year: time.Now().Year(),
 	})
@@ -100,7 +140,7 @@ func readAuthorFromGitConfig() string {
 func generateIDFromString(s string) string {
 	s = strings.TrimSpace(strings.ToLower(s))
 	s = strings.Map(func(r rune) rune {
-		if r < 'a' || r > 'z' {
+		if (r < 'a' || r > 'z') && (r < '0' || r > '9') {
 			return '-'
 		}
 		return r
@@ -117,22 +157,46 @@ func printSuccessLine(s string, args ...string) {
 	fmt.Println(strings.Join(parts, " "))
 }
 
-func createStep(inventory Inventory) error {
+func stepDirAndRepoNameFromID(stepID string) string {
+	return "bitrise-step-" + stepID
+}
+
+func createStep(inventory InventoryModel) error {
 	fmt.Println()
 
 	// create directory
-	stepDirPth, err := pathutil.AbsPath("bitrise-step-" + inventory.ID)
-	if err != nil {
-		return errors.Wrap(err, "Failed to get absolute path for step directory")
+	stepDirAbsPth := ""
+	if inventory.ToolkitType == toolkitTypeBash {
+		baseDirPath := stepDirAndRepoNameFromID(inventory.ID)
+		absPth, err := pathutil.AbsPath(baseDirPath)
+		if err != nil {
+			return errors.Wrapf(err, "Failed to get absolute path for step directory (%s)", baseDirPath)
+		}
+		stepDirAbsPth = absPth
+	} else if inventory.ToolkitType == toolkitTypeGo {
+		gopath := os.Getenv("GOPATH")
+		if len(gopath) < 1 {
+			// no GOPATH env set - use "${HOME}/go", which is the default GOPATH since Go 1.8
+			gopath = filepath.Join(pathutil.UserHomeDir(), "go")
+		}
+		baseDirPath := filepath.Join(gopath, "src", inventory.GoToolkitInventory.PackageID)
+
+		absPth, err := pathutil.AbsPath(baseDirPath)
+		if err != nil {
+			return errors.Wrapf(err, "Failed to get absolute path for step directory (%s)", baseDirPath)
+		}
+		stepDirAbsPth = absPth
+	} else {
+		return errors.Errorf("Invalid Toolkit Type: %s", inventory.ToolkitType)
 	}
 
-	printInfoLine("Creating Step directory at:", stepDirPth)
-	if exists, err := pathutil.IsPathExists(stepDirPth); err != nil {
+	printInfoLine("Creating Step directory at:", stepDirAbsPth)
+	if exists, err := pathutil.IsPathExists(stepDirAbsPth); err != nil {
 		return errors.Wrap(err, "Failed to check whether step dir already exists")
 	} else if exists {
-		return errors.Errorf("Directory (%s) already exists!", stepDirPth)
+		return errors.Errorf("Directory (%s) already exists!", stepDirAbsPth)
 	}
-	if err := os.Mkdir(stepDirPth, 0755); err != nil {
+	if err := os.MkdirAll(stepDirAbsPth, 0755); err != nil {
 		return errors.Wrap(err, "Failed to create step directory")
 	}
 
@@ -144,38 +208,38 @@ func createStep(inventory Inventory) error {
 	}{
 		{
 			TemplatePath: "README.md.gotemplate",
-			FilePath:     filepath.Join(stepDirPth, "README.md"),
+			FilePath:     filepath.Join(stepDirAbsPth, "README.md"),
 		},
 		{
 			TemplatePath: "LICENSE.gotemplate",
-			FilePath:     filepath.Join(stepDirPth, "LICENSE"),
+			FilePath:     filepath.Join(stepDirAbsPth, "LICENSE"),
 		},
 		{
 			TemplatePath: "gitignore.gotemplate",
-			FilePath:     filepath.Join(stepDirPth, ".gitignore"),
+			FilePath:     filepath.Join(stepDirAbsPth, ".gitignore"),
 		},
 		{
 			TemplatePath: "step.yml.gotemplate",
-			FilePath:     filepath.Join(stepDirPth, "step.yml"),
+			FilePath:     filepath.Join(stepDirAbsPth, "step.yml"),
 		},
 		{
 			TemplatePath: "bitrise.yml.gotemplate",
-			FilePath:     filepath.Join(stepDirPth, "bitrise.yml"),
+			FilePath:     filepath.Join(stepDirAbsPth, "bitrise.yml"),
 		},
 		{
 			TemplatePath: "bitrise.secrets.yml.gotemplate",
-			FilePath:     filepath.Join(stepDirPth, ".bitrise.secrets.yml"),
+			FilePath:     filepath.Join(stepDirAbsPth, ".bitrise.secrets.yml"),
 		},
 		// Toolkit: Bash
 		{
 			TemplatePath:  "bash/step.sh.gotemplate",
-			FilePath:      filepath.Join(stepDirPth, "step.sh"),
+			FilePath:      filepath.Join(stepDirAbsPth, "step.sh"),
 			ToolkitFilter: toolkitTypeBash,
 		},
 		// Toolkit: Go
 		{
 			TemplatePath:  "go/main.go.gotemplate",
-			FilePath:      filepath.Join(stepDirPth, "main.go"),
+			FilePath:      filepath.Join(stepDirAbsPth, "main.go"),
 			ToolkitFilter: toolkitTypeGo,
 		},
 	} {
@@ -192,16 +256,16 @@ func createStep(inventory Inventory) error {
 
 	fmt.Println()
 	fmt.Println(colorstring.Yellow("Initializing git repository in step directory ..."))
-	if err := initGitRepoAtPath(stepDirPth); err != nil {
+	if err := initGitRepoAtPath(stepDirAbsPth); err != nil {
 		return errors.Wrap(err, "Failed to initialize git repository in step directory")
 	}
 
 	fmt.Println()
 	printSuccessLine("Step is ready!")
 	fmt.Println()
-	fmt.Println("You can find it at:", stepDirPth)
+	fmt.Println("You can find it at:", stepDirAbsPth)
 	fmt.Println()
-	fmt.Println("TIP:", colorstring.Yellow("cd"), "into", colorstring.Yellow(stepDirPth), "and run",
+	fmt.Println("TIP:", colorstring.Yellow("cd"), "into", colorstring.Yellow(stepDirAbsPth), "and run",
 		colorstring.Yellow("bitrise run test"), "for a quick test drive!")
 
 	return nil
@@ -215,21 +279,30 @@ func initGitRepoAtPath(dirPth string) error {
 	return nil
 }
 
-func evaluateTemplateAndWriteToFile(filePth, templatePth string, inventory Inventory) error {
+func evaluateTemplate(templatePth string, inventory InventoryModel) (string, error) {
 	templatesBox, err := rice.FindBox("templates")
 	if err != nil {
-		return errors.Wrap(err, "Failed to find templates dir/box")
+		return "", errors.Wrap(err, "Failed to find templates dir/box")
 	}
 
 	templateContent, err := templatesBox.String(templatePth)
 	if err != nil {
-		return errors.Wrapf(err, "Failed to read %s template", templatePth)
+		return "", errors.Wrapf(err, "Failed to read %s template", templatePth)
 	}
-	cont, err := templateutil.EvaluateTemplateStringToString(templateContent, inventory, template.FuncMap{})
+	evaluatedContent, err := templateutil.EvaluateTemplateStringToString(templateContent, inventory, template.FuncMap{})
 	if err != nil {
-		return errors.Wrapf(err, "Failed to evaluate template %s", templatePth)
+		return "", errors.Wrapf(err, "Failed to evaluate template %s", templatePth)
 	}
-	if err := fileutil.WriteStringToFile(filePth, cont); err != nil {
+	return evaluatedContent, nil
+}
+
+func evaluateTemplateAndWriteToFile(filePth, templatePth string, inventory InventoryModel) error {
+	evaluatedContent, err := evaluateTemplate(templatePth, inventory)
+	if err != nil {
+		return errors.Wrap(err, "Failed to evaluate template")
+	}
+
+	if err := fileutil.WriteStringToFile(filePth, evaluatedContent); err != nil {
 		return errors.Wrapf(err, "Failed to write evaluated template into file (%s)", filePth)
 	}
 	return nil
--- create/create_test.go
+++ create/create_test.go
@@ -15,4 +15,39 @@ func Test_generateIDFromString(t *testing.T) {
 	require.Equal(t, "a-simple-test", generateIDFromString("--A simple.Test.    --"))
 	require.Equal(t, "a-simple-test", generateIDFromString("    --A simple.Test.    --      "))
 	require.Equal(t, "a--simple-test", generateIDFromString("A  simple Test"))
+	//
+	require.Equal(t, "a-simple-test-2", generateIDFromString("A simple test 2"))
+}
+
+func Test_evaluateTemplate(t *testing.T) {
+	const (
+		invAuthor         = "UT Author"
+		invTitle          = "UT Test Step"
+		invID             = "ut-test-step"
+		invSummary        = "UT summary"
+		invDescription    = "UT description."
+		invPrimaryTypeTag = "test"
+	)
+
+	t.Log("Go toolkit - step.yml template test")
+	{
+		evaluatedContent, err := evaluateTemplate("step.yml.gotemplate", InventoryModel{
+			Author:         invAuthor,
+			Title:          invTitle,
+			ID:             invID,
+			Summary:        invSummary,
+			Description:    invDescription,
+			PrimaryTypeTag: invPrimaryTypeTag,
+			Year:           2017,
+			//
+			ToolkitType: toolkitTypeGo,
+			GoToolkitInventory: GoToolkitInventoryModel{
+				PackageID: "github.com/bitrise-io/bitrise-step-" + invID,
+			},
+		})
+		require.NoError(t, err)
+		require.Contains(t, evaluatedContent, `toolkit:
+  go:
+    package_name: github.com/bitrise-io/bitrise-step-ut-test-step`)
+	}
 }
