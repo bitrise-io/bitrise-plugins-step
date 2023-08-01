464efba6c4dc5de120b35d73d8b122ef0dcc5979
Olivér Falvai <ofalvai@gmail.com>
2023-08-01 14:57:38 +0200

    Fix version override in release build

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION is the main CLI version number. It's defined at build time using -ldflags
-const VERSION = "0.99.999-development"
+var VERSION = "0.99.999-development"
 
 // BuildNumber ...
 var BuildNumber = ""
